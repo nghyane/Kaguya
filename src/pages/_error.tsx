b006aa446ce4b27033e56905de1000d7ebf94d8c
Nghyane <hoangvananhnghia99@gmail.com>
2022-11-06 06:41:11 +0700

    Update _error.tsx

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -23,13 +23,7 @@ const ErrorPage: NextPage<CustomErrorProps, CustomErrorProps> = ({
 }) => {
   const { t } = useTranslation("_error_page");
 
-  if (!hasGetInitialPropsRun && err && !isDev) {
-    // getInitialProps is not called in case of
-    // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
-    // err via _app.js so it can be captured
-    Sentry.captureException(err);
-    // Flushing is not required in this case as it only happens on the client
-  }
+
 
   return (
     <div className="relative w-full min-h-screen flex items-center">
